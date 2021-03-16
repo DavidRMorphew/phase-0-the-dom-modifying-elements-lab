062005360eebd8fb40e7b06cee07dc3227c0688a
DavidRMorphew <morphewd@umich.edu>
2021-03-16 16:42:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory'
+newHeader.innerHTML = 'David Morphew is the champion'
